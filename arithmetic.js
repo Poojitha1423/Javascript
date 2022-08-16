e856894b9613a15090f0ad7a11dbb918cdc98da4
Poojitha <poojitha032320@gmail.com>
2022-08-16 16:27:29 +0530

    operators

--- arithmetic.js
+++ arithmetic.js
@@ -2,3 +2,30 @@ var x =8;
 var y =5;
 var z =x+y;
 console.log(z)
+
+
+var x = 6;
+var y = 9;
+var z =x-y;
+console.log(z)
+
+var x =5;
+var y =3;
+var z =x*y;
+console.log(z)
+  
+var x =7;
+var y =4;
+var z =x/y;
+console.log(z)
+
+var x = 8;
+let y = 9;
+let z = x**y;
+console.log(z)
+
+
+ var x = 23;
+  y= a++;
+console.log("x: " +x);
+console.log("y: " +y);
